504b265e5a8912bf4c23156a7149c10ed1e5da73
Андрей Травин <joker.travin@yandex.ru>
2020-04-13 03:41:00 +0300

    Нормальный прототип

--- БТРИ/БТРИ.cpp
+++ БТРИ/БТРИ.cpp
@@ -1,10 +1,14 @@
 ﻿// БТРИ.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+// НАДО ДОБАВИТЬ
+// 1) Чтобы программа сама искала куда добавлять число
+// 2) Чтобы связи детей с родственниками добавлялилсь и делились правильно
+// 3) Нормальный поиск элементов и указатель головы передвижной
+// 4) Удаление
 
 #include <iostream>
 using namespace std;
 
-const int n = 5;               //n - максимальное количество УКАЗАТЕЛЕЙ в листочке
+const int n = 6;               //n - максимальное количество УКАЗАТЕЛЕЙ в листочке
 class Leaf
 {
 public:
@@ -15,7 +19,11 @@ public:
 public:
     Leaf(int);                  //для первого создания 
     void addVal(int);           //добавление значения
-    void newLeaf();          //создание нового листочка с образованием взаимных связей
+    void newLeaf();             //создание нового листочка с образованием взаимных связей
+    Leaf* searchRoot();         //ищет корень
+    Leaf* searchVal(int);
+    int childNum();
+    void addNewVal(int);
     void Show();
 };
 
@@ -27,28 +35,46 @@ Leaf :: Leaf(int initial)
 
 void Leaf :: addVal(int what)
 {
-        for (int i = 0; i < this->quantity; i++)
-            if (what <= this->Value[i])
-            {
+    for (int i = 0; i < this->quantity; i++)
+        if (what <= this->Value[i])
+        {
                 for (int j = this->quantity; j > i; j--)
                     this->Value[j] = this->Value[j - 1];
                 this->Value[i] = what;
                 break;
-            }
-        if (what > this->Value[this->quantity]) this->Value[quantity] = what;
-        this->quantity++;
-        if (this->quantity == n) { this->newLeaf(); return; }
+        }
+    if (what > this->Value[this->quantity]) this->Value[this->quantity] = what;
+    this->quantity++;
+    if (this->quantity == n) { this->newLeaf(); return; }
 }
 
 
 void Leaf::newLeaf()
 {
     int center = n/2;                                                               //узнаём индекс элемента посередине
-    if (this->Parent != NULL) this->Parent->addVal(this->Value[center]);            //если есть предок то добавляем в предок
+    Leaf* Brother = new Leaf(this->Value[center + 1]);                          //создаём брата для листа, в основе элмент после серединного
+    if (this->Parent != NULL)
+    {
+        this->Parent->addVal(this->Value[center]);                                  //если есть предок то добавляем в предок
+        Brother->Parent = this->Parent;
+        int ChildNum = this->childNum();
+        for (int i = n + 1; i > ChildNum + 1; i--)
+            this->Parent->Child[i] = this->Parent->Child[i - 1];
+        this->Parent->Child[ChildNum + 1] = Brother;
+
+        this->Value[center] = 0;
+        this->Value[center + 1] = 0;
+        for (int i = center + 2; i < this->quantity; i++)                           //переносим остаток значений в брата
+        {
+            Brother->addVal(this->Value[i]);
+            this->Value[i] = 0;
+            this->quantity--;
+        }
+        this->quantity = this->quantity - 2;
+    }
     else
     {
         Leaf* Batya = new Leaf(this->Value[center]);                                //создаём нового предка, в основе которого серединный элемент
-        Leaf* Brother = new Leaf(this->Value[center + 1]);                          //создаём брата для листа, в основе элмент после серединного
         this->Parent = Batya;                                                       //указатели на родителей
         Brother->Parent = Batya;
         this->Value[center] = 0;
@@ -71,6 +97,56 @@ void Leaf::newLeaf()
     
 }
 
+Leaf* Leaf :: searchRoot()
+{
+    Leaf* lost = this;
+    while (lost->Parent != NULL)
+        lost = lost->Parent;
+    return(lost);
+}
+
+Leaf* Leaf::searchVal(int what)
+{
+    Leaf* found = this->searchRoot();
+    while (found->Child[0] != NULL)
+    {
+        for (int i = 0; i < found->quantity; i++)
+        {
+            if (what <= found->Value[i]) found = found->Child[i];
+            break;
+        }
+    if (what > found->Value[found->quantity - 1]) found = found->Child[found->quantity];
+    }
+    return(found);
+}
+
+int Leaf::childNum()
+{
+    Leaf* buff;
+    int i = 0;
+    if (this->Parent != NULL)
+        buff = this->Parent;
+    else return(0);
+    while (buff->Child[i] != this) i++;
+    return i;
+}
+
+void Leaf::addNewVal(int what)
+{
+    Leaf* found = this->searchVal(what);
+    for (int i = 0; i < found->quantity; i++)
+       if (what <= found->Value[i])
+       {
+               for (int j = found->quantity; j > i; j--)
+                   found->Value[j] = found->Value[j - 1];
+               found->Value[i] = what;
+               break;
+       }
+   if (what > found->Value[found->quantity]) found->Value[found->quantity] = what;
+   found->quantity++;
+   if (found->quantity == n) { found->newLeaf(); return; }
+}
+
 void Leaf::Show()
 {
     for (int i = 0; i < this->quantity; i++) printf("%d ", this->Value[i]);
@@ -80,19 +156,22 @@ void Leaf::Show()
 int main()
 {
     Leaf Root(10);
-    Root.addVal(8);
-    Root.addVal(60);
-    Root.addVal(6);
-    Root.addVal(9);
-    printf("%d\n", Root.quantity);
+    Root.addNewVal(8);
+    Root.addNewVal(60);
+    Root.addNewVal(6);
+    Root.addNewVal(9);
+    Root.addNewVal(40);
+    Root.addNewVal(4);
+    Root.addNewVal(3);
+    Root.addNewVal(2);
+    Leaf* Main = Root.searchRoot();
+    Main->Child[1]->Show();
+    printf("\n");
     Root.Show();
     printf("\n");
-    printf("%d\n", Root.Parent->quantity);
     Root.Parent->Show();
     printf("\n");
-    printf("%d\n", Root.Parent->Child[1]->quantity);
-    Root.Parent->Child[1]->Show();
-    return(0);
+    Root.Parent->Child[2]->Show();
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
